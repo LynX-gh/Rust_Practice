57e28829fc5b37f1f401d8a566c09d050500551c
Khush Ramani <76166621+LynX-gh@users.noreply.github.com>
2024-03-11 00:08:03 +0530

    Rust Formatted Print
    Signed-off-by: Khush Ramani <76166621+LynX-gh@users.noreply.github.com>

---
+++ hello-world/src/main.rs
@@ -0,0 +1,12 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    println!("Hello World!");
+    say(&message, width, &mut writer).unwrap();
+}
